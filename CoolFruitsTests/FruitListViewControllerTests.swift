8527332fc8e557c428a727762aab3a5647b81311
Mario Alberto Barragán Espinosa <mariosdev05@gmail.com>
2022-09-10 19:04:33 -0500

    Add assert helpers for FruitListViewControllerTests

--- CoolFruitsTests/FruitListViewControllerTests.swift
+++ CoolFruitsTests/FruitListViewControllerTests.swift
@@ -24,10 +24,10 @@ final class FruitListViewControllerTests: XCTestCase {
         let (sut, loader) = makeSUT()
         
         sut.loadViewIfNeeded()
-        XCTAssertEqual(sut.numberOfRenderedFruits(), 0)
+        assertThat(sut, isRendering: [])
         
         loader.completeFruitsLoading(with: [fruit0, fruit1], at: 0)
-        XCTAssertEqual(sut.numberOfRenderedFruits(), 2)
+        assertThat(sut, isRendering: [fruit0, fruit1])
     }
     
     // MARK: - Helpers
@@ -44,10 +44,52 @@ final class FruitListViewControllerTests: XCTestCase {
         
         return (sut, loader)
     }
+    
+    func assertThat(_ sut: FruitListViewController, 
+                    isRendering fruits: [FruitModel], 
+                    file: StaticString = #file, 
+                    line: UInt = #line) {
+        
+        guard sut.numberOfRenderedFruits() == fruits.count else {
+            return XCTFail("Expected \(fruits.count) fruits, got \(sut.numberOfRenderedFruits()) instead.", file: file, line: line)
+        }
+
+        fruits.enumerated().forEach { index, fruit in
+            assertThat(sut, hasViewConfiguredFor: fruit, 
+                       at: index, 
+                       file: file, 
+                       line: line)
+        }        
+    }
+    
+    func assertThat(_ sut: FruitListViewController, 
+                    hasViewConfiguredFor fruit: FruitModel, 
+                    at index: Int, 
+                    file: StaticString = #file, 
+                    line: UInt = #line) {
+        let view = sut.feedFruitCell(at: index)
+
+        guard let cell = view as? FruitCell else {
+            return XCTFail("Expected \(FruitCell.self) instance, got \(String(describing: view)) instead", file: file, line: line)
+        }
+        let isCitrus = fruit.genus == "Citrus"
+        let showedFruitName = "\(isCitrus ? " * " : "") \(fruit.name ?? "")"
+        
+        XCTAssertEqual(cell.fruitNameLabel.text, showedFruitName, "Expected fruit name text to be \(String(describing: showedFruitName)) for  view at index (\(index))", file: file, line: line)
+    }
 }
 
 extension FruitListViewController {
     func numberOfRenderedFruits() -> Int {
         return fruitsTableView.numberOfRows(inSection: 0)
+    }
+    
+    func feedFruitCell(at row: Int) -> UITableViewCell? {
+        guard numberOfRenderedFruits() > row else {
+            return nil
+        }
+        let ds = fruitsTableView.dataSource
+        let index = IndexPath(row: row, section: 0)
+        return ds?.tableView(fruitsTableView, cellForRowAt: index)
     }
 }
